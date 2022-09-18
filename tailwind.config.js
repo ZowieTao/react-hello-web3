c6335c7763559106ff01c8db0722cded1c659c8c
root <root@DESKTOP-P6I7SPN.localdomain>
2022-09-18 16:53:14 +0800

    First Experience

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,tsx,jsx}"],
+  theme: {
+    letterSpacing: {
+      tight: "-0.15em",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
